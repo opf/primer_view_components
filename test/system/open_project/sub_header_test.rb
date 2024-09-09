2067dadf310bbb64f847329c0c625be6a6687da9
Dombi Attila <83396+dombesz@users.noreply.github.com>
2024-09-09 13:25:19 +0300

    Fix tests

--- test/system/open_project/sub_header_test.rb
+++ test/system/open_project/sub_header_test.rb
@@ -19,24 +19,20 @@ class IntegrationOpenProjectSubHeaderTest < System::TestCase
   def test_clear_button_functionality
     visit_preview(:playground, show_clear_button: true)
     # no clear button with empty value
-    assert_no_selector(".FormControl-input-wrap--trailingAction")
     assert_no_selector("button.FormControl-input-trailingAction")
 
     fill_in "filter", with: "value"
 
     # Clear the field with the "x" button
     find(".FormControl-input-trailingAction").click
-    assert_no_selector(".FormControl-input-wrap--trailingAction")
     assert_no_selector("button.FormControl-input-trailingAction")
 
     fill_in "filter", with: "value"
-    assert_selector(".FormControl-input-wrap--trailingAction")
     assert_selector("button.FormControl-input-trailingAction")
 
     # Clear the field with backspace
     filter_field = find_field "filter"
     "value".length.times { filter_field.send_keys [:backspace] }
-    assert_no_selector(".FormControl-input-wrap--trailingAction")
     assert_no_selector("button.FormControl-input-trailingAction")
   end
 end
