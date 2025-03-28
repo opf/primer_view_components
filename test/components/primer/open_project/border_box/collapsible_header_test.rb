8903b822ee2289772a7be60b660970b9fc14b50e
Mir Bhatia <m.bhatia@openproject.com>
2025-03-28 15:10:46 +0100

    Add tests

--- test/components/primer/open_project/border_box/collapsible_header_test.rb
+++ test/components/primer/open_project/border_box/collapsible_header_test.rb
@@ -2,12 +2,28 @@
 
 require "components/test_helper"
 
-class PrimerOpenProjectBorderBox::CollapsibleHeaderTest < Minitest::Test
+class Primer::OpenProject::BorderBox::CollapsibleHeaderTest < Minitest::Test
   include Primer::ComponentTestHelpers
 
-  def test_renders
-    render_inline(Primer::BorderBox::CollapsibleHeader.new)
+  def test_renders_default
+    render_preview(:default)
 
-    assert_text("Add a test here")
+    assert_selector(".CollapsibleHeader", text: "Default title")
+    assert_selector("svg.up-icon")
+    assert_selector("svg.down-icon.d-none")
   end
+
+  def does_not_render_without_title_and_box
+    render_inline(Primer::OpenProject::BorderBox::CollapsibleHeader.new())
+
+    refute_component_rendered
+  end
+
+  def does_not_render_without_valid_box
+    render_inline(Primer::OpenProject::BorderBox::CollapsibleHeader.new(title: "Test title", box: "Some component"))
+
+    refute_component_rendered
+  end
+
+
 end
