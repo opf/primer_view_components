f2a605cf312512ac4fea65fb4b4e685c20f5f3ec
Henriette Darge <h.darge@openproject.com>
2025-05-08 12:36:39 +0200

    Create a custom Button class to enforce leading icons on all buttons

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -221,6 +221,7 @@ class PrimerComponentTest < Minitest::Test
       "Primer::OpenProject::PageHeader::Menu",
       "Primer::OpenProject::PageHeader::Dialog",
       "Primer::OpenProject::PageHeader::Title",
+      "Primer::OpenProject::SubHeader::Button",
       "Primer::OpenProject::SubHeader::ButtonGroup",
       "Primer::OpenProject::SubHeader::SegmentedControl",
       "Primer::OpenProject::SidePanel::Section",
