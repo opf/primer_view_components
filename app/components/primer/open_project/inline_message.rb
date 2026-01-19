c01444418fda17fc6d3595d4450343e935373efc
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-01-19 15:44:20 +0100

    create the inline message component

---
+++ app/components/primer/open_project/inline_message.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Primer
+  module OpenProject
+    # A simple component to render warning text.
+    #
+    # The warning text is rendered in the "attention" Primer color and
+    # uses a leading alert Octicon for additional emphasis. This component
+    # is designed to be used "inline", e.g. table cells, and in places
+    # where a Banner component might be overkill.
+    class InlineMessage < Primer::Component
+      SCHEME_ICON_MAPPINGS = {
+        warning: :alert,
+        critical: :alert,
+        success: :"check-circle",
+        unavailable: :alert
+      }.freeze
+      private_constant :SCHEME_ICON_MAPPINGS
+      SCHEME_OPTIONS = SCHEME_ICON_MAPPINGS.keys
+
+      SCHEME_SMALL_ICON_MAPPINGS = {
+        warning: :"alert-fill",
+        critical: :"alert-fill",
+        success: :"check-circle-fill",
+        unavailable: :"alert-fill"
+      }.freeze
+      private_constant :SCHEME_SMALL_ICON_MAPPINGS
+      DEFAULT_SIZE = :medium
+      SIZE_OPTIONS = [:small, DEFAULT_SIZE].freeze
+
+      # @param scheme [Symbol] <%= one_of(SCHEME_OPTIONS) %>
+      # @param size [Symbol] <%= one_of(SIZE_OPTIONS) %>
+      # @param system_arguments [Hash] <%= link_to_system_arguments_docs %>
+      def initialize(scheme:, size: DEFAULT_SIZE, **system_arguments) # rubocop:disable Lint/MissingSuper
+        resolved_scheme = fetch_or_fallback(SCHEME_OPTIONS, scheme)
+        resolved_size   = fetch_or_fallback(SIZE_OPTIONS, size, DEFAULT_SIZE)
+
+        @system_arguments = system_arguments
+        @system_arguments[:tag] ||= :div
+        @system_arguments[:classes] = class_names(
+          @system_arguments[:classes],
+          "InlineMessage"
+        )
+        @system_arguments[:data] = merge_data(
+          @system_arguments,
+          { data: { size: resolved_size, variant: resolved_scheme } }
+        )
+
+        @icon_arguments = { classes: "InlineMessageIcon" }
+        if resolved_size == :small
+          @icon_arguments[:icon] = SCHEME_SMALL_ICON_MAPPINGS[resolved_scheme]
+          @icon_arguments[:size] = :xsmall
+        else
+          @icon_arguments[:icon] = SCHEME_ICON_MAPPINGS[resolved_scheme]
+        end
+      end
+
+      def render?
+        content.present?
+      end
+    end
+  end
+end
---
+++ test/components/primer/open_project/inline_message_test.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require "components/test_helper"
+
+class PrimerOpenProjectInlineMESSAGETest < Minitest::Test
+  include Primer::ComponentTestHelpers
+
+  def test_renders_with_content
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :warning)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage")
+    assert_selector(".InlineMessageIcon")
+    assert_text("Important Message")
+  end
+
+  def test_renders_nothing_with_blank_content
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :warning)) do
+      " "
+    end
+
+    assert_no_selector(".InlineMessage")
+    assert_no_text("Important Message")
+  end
+
+  def test_default_size_warning_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :warning)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='medium'][data-variant='warning']")
+    assert_selector("svg.octicon-alert")
+  end
+
+  def test_default_size_critical_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :critical)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='medium'][data-variant='critical']")
+    assert_selector("svg.octicon-alert")
+  end
+
+  def test_default_size_success_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :success)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='medium'][data-variant='success']")
+    assert_selector("svg.octicon-check-circle")
+  end
+
+  def test_default_size_unavailable_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :unavailable)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='medium'][data-variant='unavailable']")
+    assert_selector("svg.octicon-alert")
+  end
+
+  def test_small_size_warning_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :warning, size: :small)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='small'][data-variant='warning']")
+    assert_selector("svg.octicon-alert-fill")
+  end
+
+  def test_small_size_critical_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :critical, size: :small)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='small'][data-variant='critical']")
+    assert_selector("svg.octicon-alert-fill")
+  end
+
+  def test_small_size_success_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :success, size: :small)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='small'][data-variant='success']")
+    assert_selector("svg.octicon-check-circle-fill")
+  end
+
+  def test_small_size_unavailable_scheme
+    render_inline(Primer::OpenProject::InlineMessage.new(scheme: :unavailable, size: :small)) do
+      "Important Message"
+    end
+
+    assert_selector(".InlineMessage[data-size='small'][data-variant='unavailable']")
+    assert_selector("svg.octicon-alert-fill")
+  end
+end
