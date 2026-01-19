e7feee2c64fc6823f5a77daae7992c53173f3eae
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-01-19 16:04:10 +0100

    Preview for inline message

---
+++ previews/primer/open_project/inline_message_preview.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Primer
+  module OpenProject
+    class InlineMessagePreview < ViewComponent::Preview
+      # @label Default
+      # @snapshot
+      def default
+      end
+    end
+  end
+end
