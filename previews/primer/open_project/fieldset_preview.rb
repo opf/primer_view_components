fdbb6f6bbb8e5d2c804910583b162704684f2939
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-01-20 09:15:09 +0100

    Add a palyground preview

--- previews/primer/open_project/fieldset_preview.rb
+++ previews/primer/open_project/fieldset_preview.rb
@@ -9,6 +9,16 @@ module Primer
         render_with_template
       end
 
+      # @param legend_text [String] text
+      # @param visually_hide_legend [Boolean]
+      def playground(
+        legend_text: "Hello fieldset",
+        visually_hide_legend: true
+      )
+        render_with_template(locals: { legend_text: legend_text,
+                                       visually_hide_legend: visually_hide_legend })
+      end
+
       def with_legend_text
         render_with_template
       end
