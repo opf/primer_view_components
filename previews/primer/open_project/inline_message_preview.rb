5874755111c6b9da4f6467f82dfdc85d4838fdff
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-01-20 11:42:25 +0100

    Add playground preview

--- previews/primer/open_project/inline_message_preview.rb
+++ previews/primer/open_project/inline_message_preview.rb
@@ -7,6 +7,16 @@ module Primer
       # @snapshot
       def default
       end
+
+      # @label Playground
+      # @param scheme [Symbol] select [warning, success]
+      # @param size [Symbol] select [small, medium]
+      def playground(
+        scheme: :warning,
+        size: :medium
+      )
+        render_with_template(locals: { scheme: scheme, size: size })
+      end
     end
   end
 end
