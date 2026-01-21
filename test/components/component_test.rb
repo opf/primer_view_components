f1d4a2bbd421a04fa9b29f7fcfce3895af461764
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-01-21 08:02:53 +0100

    Add Primer::OpenProject::Fieldset to component registry tests

--- test/components/component_test.rb
+++ test/components/component_test.rb
@@ -12,6 +12,7 @@ class PrimerComponentTest < Minitest::Test
       component.with_title { "Hello world" }
       component.with_collapsible_content { "Collapse me!" }
     }],
+    [Primer::OpenProject::Fieldset, { legend_text: "My legend" }, proc { "Content" }],
     [Primer::OpenProject::BorderBox::CollapsibleHeader, { box: Primer::Beta::BorderBox.new }, proc { |component|
       component.with_title { "Hello world" }
     }],
