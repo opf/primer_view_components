1135121f86076c04a4f76856b497acc8a725ddc3
Behrokh Satarnejad <b.satarnejad@openproject.com>
2026-02-02 11:52:27 +0100

    Set correct params for column component

--- app/components/primer/open_project/data_table/column.rb
+++ app/components/primer/open_project/data_table/column.rb
@@ -12,16 +12,25 @@ module Primer
 
         attr_reader :id, :align, :field, :max_width, :min_width, :row_header, :sort_by, :width, :cell_block
 
-        renders_one :header, -> {
-          Primer::Content.new
-        }
+        renders_one :header, -> { Primer::Content.new }
 
+        # Provide a custom cell renderer.
+        #
+        # @yieldparam row [Object] The row object for the current table row
+        # @yieldreturn [Object] Renderable content for the cell
         def with_cell(&block)
           @cell_block = block
         end
 
-        # @param align The horizontal alignment of the column's content
-        # @param header Provide the name of the column. This will be rendered as a table header within the table itself
+        # @param id [String, Symbol, nil] Optional DOM id for the column
+        # @param field [Symbol, nil] Attribute/method name called on each row to render a default cell value
+        # @param align [Symbol] Horizontal alignment of the column content
+        # @param header [String, nil] Column header text (rendered in the table header)
+        # @param width [Numeric, String, Symbol, nil] Width value or one of COLUMN_WIDTH_OPTIONS
+        # @param sort_by [String, Symbol, nil] Sort key used for sorting
+        # @param row_header [Boolean] Whether this column is a row header (`<th scope="row">`)
+        # @param min_width [Numeric, String, nil] Minimum width
+        # @param max_width [Numeric, String, nil] Maximum width
         def initialize( # rubocop:disable Lint/MissingSuper
           id: nil,
           field: nil,
@@ -35,15 +44,16 @@ module Primer
         )
           @id = id
           @field = field
-
           @align = align
           @header = header
+
           @width =
             if width.is_a?(Numeric) || width.is_a?(String)
               width
             elsif width.present?
               fetch_or_fallback(COLUMN_WIDTH_OPTIONS, width)
             end
+
           @sort_by = sort_by
           @row_header = row_header
           @max_width = max_width
